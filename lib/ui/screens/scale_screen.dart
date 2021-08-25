0412fe133def1c475292892ba97df6a715c43326
Pasha Semenov <paul23093@yandex.ru>
2021-08-25 13:16:06 +0300

    Weighting streaming

--- lib/ui/screens/scale_screen.dart
+++ lib/ui/screens/scale_screen.dart
@@ -6,6 +6,7 @@ import 'package:health_tracker/constants/scale.dart';
 import 'package:health_tracker/constants/utils.dart';
 import 'package:health_tracker/model/user_device.dart';
 import 'package:health_tracker/ui/screens/profile_screen.dart';
+import 'package:health_tracker/ui/widgets/progress_indicator.dart';
 import 'package:health_tracker/ui/widgets/screen_header.dart';
 import 'package:health_tracker/model/user_product.dart';
 import 'package:health_tracker/ui/widgets/rounded_button.dart';
@@ -61,21 +62,23 @@ class _ScaleScreenState extends State<ScaleScreen> {
     // final double fats = widget.userProduct.fat * weight / 100;
     // final double carbs = widget.userProduct.carbs * weight / 100;
     // final double kcal = widget.userProduct.kcal * weight / 100;
-    return StreamBuilder<List<int>>(
+    return Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Center(
+            child: StreamBuilder<List<int>>(
         stream:
             flutterReactiveBle.subscribeToCharacteristic(scaleCharacteristic),
         builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
-          if (snapshot.hasData) {
+          if (!snapshot.hasData) {
+            return const HavkaProgressIndicator();
+          } else {
             final String valueString = utils.listIntToString(snapshot.data);
             final double weight = double.parse(valueString);
             final double protein = widget.userProduct.protein * weight / 100;
             final double fats = widget.userProduct.fat * weight / 100;
             final double carbs = widget.userProduct.carbs * weight / 100;
             final double kcal = widget.userProduct.kcal * weight / 100;
-            return Scaffold(
-                backgroundColor: Theme.of(context).backgroundColor,
-                body: Center(
-                  child: Column(
+            return Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         ScreenSubHeader(text: widget.userProduct.productName),
@@ -94,13 +97,8 @@ class _ScaleScreenState extends State<ScaleScreen> {
                             Navigator.pop(context);
                           },
                         )
-                      ]),
-                ));
-          } else {
-            return Scaffold(
-                backgroundColor: Theme.of(context).backgroundColor,
-                body: const Center(child: HavkaText(text: 'No data')));
+                      ]);
           }
-        });
+        })));
   }
 }
