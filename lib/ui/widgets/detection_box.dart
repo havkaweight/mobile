3af1cc8ba5fb6f98d029785c1f3fbb6b3d45bc7b
psemenov <pavel.semenov@exness.com>
2023-03-31 15:11:32 +0300

    Updated signin screen

---
+++ lib/ui/widgets/detection_box.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../model/data_items.dart';
+
+
+class HavkaBarChart extends CustomPainter {
+  final List<DataItem> data;
+  HavkaBarChart(this.data);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double left = size.width * 0.2;
+    final double maxValue = data.map((di) => di.value).reduce(max);
+    final int valuesCount = data.length;
+    final double barWidth = (size.width - 2 * left) / valuesCount;
+    for(final di in data) {
+      final height = di.value / maxValue * size.height / 3.0;
+      final rect = Rect.fromLTWH(left, size.height/1.5, barWidth*0.9, -height);
+      final paint = Paint()
+        ..style = PaintingStyle.fill
+        ..color = di.color;
+      canvas.drawRect(rect, paint);
+      left += barWidth;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant HavkaBarChart oldDelegate) => true;
+}
